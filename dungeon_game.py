6d156fd1ce9411f4a1a85017f0f10e5e1343d3c0
Kevin Gann <kgann@5amsolutions.com>
2015-09-19 14:56:46 -0700

    Allow the monster to move! Allow the monster and player to stay put.

--- dungeon_game.py
+++ dungeon_game.py
@@ -1,14 +1,13 @@
 import random
 
-MATRIX_X = 5
-MATRIX_Y = 5
+MATRIX_SIZE = 5
 
 chosen_cells = dict()
 
 def initialize_locations():
-    monster = get_random_unique_location("MONSTER", MATRIX_X, MATRIX_Y)
-    door = get_random_unique_location("DOOR", MATRIX_X, MATRIX_Y)
-    start = get_random_unique_location("PLAYER", MATRIX_X, MATRIX_Y)
+    monster = get_random_unique_location("MONSTER", MATRIX_SIZE, MATRIX_SIZE)
+    door = get_random_unique_location("DOOR", MATRIX_SIZE, MATRIX_SIZE)
+    start = get_random_unique_location("PLAYER", MATRIX_SIZE, MATRIX_SIZE)
 
     # return monster, door, start
     return monster, door, start
@@ -41,16 +40,32 @@ def move_player(location, move):
         chosen_cells['PLAYER'] = (current_location_x - 1, current_location_y)
     if move == 'D':
         chosen_cells['PLAYER'] = (current_location_x + 1, current_location_y)
+    if move == 'S':
+        chosen_cells['PLAYER'] = (current_location_x, current_location_y)
+
+def move_monster(location, move):
+    current_location_x = location[0]
+    current_location_y = location[1]
+    if move == 'L':
+        chosen_cells['MONSTER'] = (current_location_x, current_location_y - 1)
+    if move == 'R':
+        chosen_cells['MONSTER'] = (current_location_x, current_location_y + 1)
+    if move == 'U':
+        chosen_cells['MONSTER'] = (current_location_x - 1, current_location_y)
+    if move == 'D':
+        chosen_cells['MONSTER'] = (current_location_x + 1, current_location_y)
+    if move == 'S':
+        chosen_cells['MONSTER'] = (current_location_x, current_location_y)
 
 def get_moves(location):
-    MOVES = ['L', 'R', 'U', 'D']
+    MOVES = ['L', 'R', 'U', 'D', 'S']
     if location[1] == 0:
         MOVES.remove('L')
     if location[0] == 0:
         MOVES.remove('U')
-    if location[1] == MATRIX_Y - 1:
+    if location[1] == MATRIX_SIZE - 1:
         MOVES.remove('R')
-    if location[0] == MATRIX_X - 1:
+    if location[0] == MATRIX_SIZE - 1:
         MOVES.remove('D')
     return MOVES
 
@@ -59,14 +74,14 @@ def print_matrix():
     door_location = chosen_cells['DOOR']
     monster_location = chosen_cells['MONSTER']
 
-    MATRIX = [[0 for x in range(5)] for x in range(5)]
+    MATRIX = [[0 for x in range(MATRIX_SIZE)] for x in range(MATRIX_SIZE)]
 
     MATRIX[player_location[0]][player_location[1]] = 1
     MATRIX[door_location[0]][door_location[1]] = 2
     MATRIX[monster_location[0]][monster_location[1]] = 3
 
-    for x in list(range(MATRIX_X)):
-        for y in list(range(MATRIX_Y)):
+    for x in list(range(MATRIX_SIZE)):
+        for y in list(range(MATRIX_SIZE)):
             print("{} ".format(MATRIX[x][y]), end="")
         print("\n", end="")
 
@@ -91,6 +106,9 @@ while True:
 
     if move in get_moves(chosen_cells['PLAYER']):
         move_player(chosen_cells['PLAYER'], move)
+        monster_moves = get_moves(chosen_cells['MONSTER'])
+        monster_move = monster_moves[random.randint(0, len(monster_moves) - 1)]
+        move_monster(chosen_cells['MONSTER'], monster_move)
     else:
         print("Sorry, I don't undestand {}".format(move))
         continue
