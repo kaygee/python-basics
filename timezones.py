9774c73a1f1ea77c4c91eae44d697730c03a4837
Kevin Gann <kgann@5amsolutions.com>
2015-09-29 09:12:03 -0500

    timezones!

--- timezones.py
+++ timezones.py
@@ -1,7 +1,17 @@
-improt datetime
+import datetime
 
 pacific = datetime.timezone(datetime.timedelta(hours =- 8))
-eastern = datetime.timezone(datetime.timedelta(hours =- 5))
-naive = datetime.datetime(2015, 5, 5, 9)
+europe = datetime.timezone(datetime.timedelta(hours =+ 1))
 
-aware = datetime.datetime(2015, 5, 5, 0, tzinfo=pacific)
+# naive doesn't know its timezone.
+naive = datetime.datetime(2015, 10, 21, 4, 29)
+
+# aware knows its timezone
+hill_valley = datetime.datetime(2015, 10, 21, 4, 29, tzinfo=pacific)
+paris = datetime.datetime(2015, 10, 21, 4, 29, tzinfo=europe)
+
+paris = hill_valley.astimezone(europe)
+
+print(naive)
+print(hill_valley)
+print(paris)
